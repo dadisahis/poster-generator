7b4173cfff172bbdc5122f92c2dcb536c44423fe
Adithya Saish <adiwithmj@gmail.com>
2023-02-23 01:18:11 +0530

    sorted  form data generation issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import { posterContext } from "./context/posterContext";
 import Home from "./Pages/Home/Home";
 function App() {
+  const { state } = useContext(posterContext);
+  console.log(state);
   return (
     <div className="App">
       <Home />
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef,useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import "./form.scss";
 import logo from "../../assets/logo.jpg";
@@ -6,86 +6,107 @@ import { UploadFileRounded } from "@mui/icons-material";
 import { useEffect } from "react";
 import { posterContext } from "../../context/posterContext";
 function Form() {
-  const {state, dispath} = useContext(posterContext)
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm({mode: "all"});
-  const [posterObj,setPosterObj] = useState(state)
-  const onSubmit = (data) => {
-    console.log(data)
-    console.log("helloo")
+  const { state, dispatch } = useContext(posterContext);
+  const [posterObj, setPosterObj] = useState(state);
+  const onSubmit = () => {
+    dispatch({ type: "set_poster_obj", payload: posterObj });
   };
   const authorPicRef = useRef(null);
   const themePicRef = useRef(null);
-  const handleClick = (ref)=>{
-    console.log(ref)
-    ref.current?.click()
-  }
+  const handleClick = (ref) => {
+    console.log(ref);
+    ref.current?.click();
+  };
+  const handleChange = (e) => {
+    setPosterObj({ ...posterObj, [e.target.name]: e.target.value });
+  };
+
   const handleAuthorChange = (e) => {
-    setPosterObj({...posterObj, authorImage: e.target.files[0]})
-    
-  }
+    setPosterObj({
+      ...posterObj,
+      authorImage: URL.createObjectURL(e.target.files[0]),
+    });
+  };
   const handleImageChange = (e) => {
-    setPosterObj({...posterObj, themeImage: e.target.files[0]})
-  }
-  console.log(errors)
-  console.log(posterObj)
+    setPosterObj({
+      ...posterObj,
+      themeImage: URL.createObjectURL(e.target.files[0]),
+    });
+  };
+
+  useEffect(() => {
+    console.log(posterObj);
+  }, [posterObj]);
   return (
     <div className="form">
       <img src={logo} alt="" />
       <p>Poster Generator</p>
-      <form  className="form_container">
+      <form className="form_container">
         <label>
           <p>News Type</p>
         </label>
-        <input {...register("newsType", { required: true })} />
+        <input type="text" name="newsType" onChange={(e) => handleChange(e)} />
         <label>
           <p>News Title</p>
         </label>
-        <input {...register("newsTitle", { required: true })} />
+        <input type="text" name="title" onChange={(e) => handleChange(e)} />
         <label>
           <p>Author Name</p>
         </label>
-        <input {...register("authorName", { required: true })} />
-        <div className="upload_container" onClick={() => handleClick(authorPicRef)}>
-          <input
-          {...register("authorImage", { required: true })}
-            type="file"
-            name="authorPicFile"
-            className="fileUpload"
-            ref={authorPicRef}
-            onChange={handleAuthorChange}
-            
-          />
+        <input
+          type="text"
+          name="authorName"
+          onChange={(e) => handleChange(e)}
+        />
+        {posterObj.authorImage === "" ? (
+          <div
+            className="upload_container"
+            onClick={() => handleClick(authorPicRef)}
+          >
+            <input
+              type="file"
+              className="fileUpload"
+              ref={authorPicRef}
+              onChange={handleAuthorChange}
+            />
 
-          <UploadFileRounded />
-          <p>Upload Authors picture for the poster</p>
-        </div>
-        <div
-          className="upload_container"
-          onClick={() => handleClick(themePicRef)}
-        >
-          <input
-          {...register("themeImage", { required: true })}
-            type="file"
-            name="themeFile"
-            className="fileUpload"
-            ref={themePicRef}
-            onChange={handleImageChange}
-            
-            
-          />
-          <UploadFileRounded />
-          <p>Upload picture for the poster</p>
-        </div>
-        <button type="button" onClick={handleSubmit(onSubmit)}>Generate</button>
+            <UploadFileRounded />
+            <p>Upload Authors picture for the poster</p>
+          </div>
+        ) : (
+          <div className="img_container">
+            <img src={posterObj.authorImage} alt="" />
+          </div>
+        )}
+        {posterObj.themeImage === "" ? (
+          <div
+            className="upload_container"
+            onClick={() => handleClick(themePicRef)}
+          >
+            <input
+              type="file"
+              className="fileUpload"
+              ref={themePicRef}
+              onChange={handleImageChange}
+            />
+
+            <>
+              <UploadFileRounded />
+              <p>Upload picture for the poster</p>
+            </>
+          </div>
+        ) : (
+          <div className="img_container">
+            <img src={posterObj.themeImage} alt="" />
+          </div>
+        )}
+        <button type="button" onClick={onSubmit}>
+          Generate
+        </button>
       </form>
       {/* {errors.map(obj => {
         <p>{obj}</p>
       })} */}
-      
     </div>
   );
 }
