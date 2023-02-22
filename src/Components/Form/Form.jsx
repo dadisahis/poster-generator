1bcf647d0e6a7603f434c9c9f0858d3e04c9260a
Adithya Saish <adiwithmj@gmail.com>
2023-02-22 09:47:43 +0530

    UI changes to form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,28 +1,71 @@
-import React from 'react'
-import {useForm } from "react-hook-form"
-import "./form.scss"
-import logo from "../../assets/logo.jpg"
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
+import "./form.scss";
+import logo from "../../assets/logo.jpg";
+import { UploadFileRounded } from "@mui/icons-material";
 function Form() {
-    const {handleSubmit,register, formState: {errors} } = useForm()
-    const onSubmit = ()=> console.log("hello")
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  const authorPicRef = useRef(null);
+  const themePicRef = useRef(null);
+
+  const handleClick = () => {
+    if (authorPicRef.current) {
+      authorPicRef.current.click();
+    }
+  };
   return (
-    <div className='form'>
-        <img src={logo} />
-        <p>Poster Generator</p>
-        <form onClick = {handleSubmit(onSubmit)} className="form_container">
-            <label><p>News Type</p></label>
-            <input {...register("newsType",{required: true})} />
-            <label><p>News Title</p></label>
-            <input {...register("title",{required: true})} />
-            <label><p>Author Name</p></label>
-            <input {...register("authorName",{required: true})} />
-            <input type="file" {...register("authorImage", {required: true})} />
+    <div className="form">
+      <img src={logo} alt="" />
+      <p>Poster Generator</p>
+      <form onClick={handleSubmit(onSubmit)} className="form_container">
+        <label>
+          <p>News Type</p>
+        </label>
+        <input {...register("newsType", { required: true })} />
+        <label>
+          <p>News Title</p>
+        </label>
+        <input {...register("title", { required: true })} />
+        <label>
+          <p>Author Name</p>
+        </label>
+        <input {...register("authorName", { required: true })} />
+        <div className="upload_container" onClick={() => handleClick()}>
+          <input
+            type="file"
+            name="authorPicFile"
+            className="fileUpload"
+            ref={authorPicRef}
+            onChange={() => console.log("hellp")}
+            {...register("authorImage", { required: true })}
+          />
 
-        </form>
-        <button >Generate</button>
-
+          <UploadFileRounded />
+          <p>Upload Authors picture for the poster</p>
+        </div>
+        <div
+          className="upload_container"
+          // onClick={() => handleClick(themePicRef)}
+        >
+          <input
+            type="file"
+            name="themeFile"
+            className="fileUpload"
+            {...register("themeImage", { required: true })}
+            ref={themePicRef}
+          />
+          <UploadFileRounded />
+          <p>Upload picture for the poster</p>
+        </div>
+      </form>
+      <button>Generate</button>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
