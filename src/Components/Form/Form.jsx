dee7a8576b7b4ebbe204467ebbb764aac7421b57
dadisahis <adiwithmj@gmail.com>
2023-02-22 18:57:09 +0530

    fixed upload image issue

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,28 +1,42 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef,useState } from "react";
 import { useForm } from "react-hook-form";
 import "./form.scss";
 import logo from "../../assets/logo.jpg";
 import { UploadFileRounded } from "@mui/icons-material";
+import { useEffect } from "react";
+import { posterContext } from "../../context/posterContext";
 function Form() {
+  const {state, dispath} = useContext(posterContext)
   const {
     handleSubmit,
     register,
     formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
+  } = useForm({mode: "all"});
+  const [posterObj,setPosterObj] = useState(state)
+  const onSubmit = (data) => {
+    console.log(data)
+    console.log("helloo")
+  };
   const authorPicRef = useRef(null);
   const themePicRef = useRef(null);
-
-  const handleClick = () => {
-    if (authorPicRef.current) {
-      authorPicRef.current.click();
-    }
-  };
+  const handleClick = (ref)=>{
+    console.log(ref)
+    ref.current?.click()
+  }
+  const handleAuthorChange = (e) => {
+    setPosterObj({...posterObj, authorImage: e.target.files[0]})
+    
+  }
+  const handleImageChange = (e) => {
+    setPosterObj({...posterObj, themeImage: e.target.files[0]})
+  }
+  console.log(errors)
+  console.log(posterObj)
   return (
     <div className="form">
       <img src={logo} alt="" />
       <p>Poster Generator</p>
-      <form onClick={handleSubmit(onSubmit)} className="form_container">
+      <form  className="form_container">
         <label>
           <p>News Type</p>
         </label>
@@ -30,19 +44,20 @@ function Form() {
         <label>
           <p>News Title</p>
         </label>
-        <input {...register("title", { required: true })} />
+        <input {...register("newsTitle", { required: true })} />
         <label>
           <p>Author Name</p>
         </label>
         <input {...register("authorName", { required: true })} />
-        <div className="upload_container" onClick={() => handleClick()}>
+        <div className="upload_container" onClick={() => handleClick(authorPicRef)}>
           <input
+          {...register("authorImage", { required: true })}
             type="file"
             name="authorPicFile"
             className="fileUpload"
             ref={authorPicRef}
-            onChange={() => console.log("hellp")}
-            {...register("authorImage", { required: true })}
+            onChange={handleAuthorChange}
+            
           />
 
           <UploadFileRounded />
@@ -50,20 +65,27 @@ function Form() {
         </div>
         <div
           className="upload_container"
-          // onClick={() => handleClick(themePicRef)}
+          onClick={() => handleClick(themePicRef)}
         >
           <input
+          {...register("themeImage", { required: true })}
             type="file"
             name="themeFile"
             className="fileUpload"
-            {...register("themeImage", { required: true })}
             ref={themePicRef}
+            onChange={handleImageChange}
+            
+            
           />
           <UploadFileRounded />
           <p>Upload picture for the poster</p>
         </div>
+        <button type="button" onClick={handleSubmit(onSubmit)}>Generate</button>
       </form>
-      <button>Generate</button>
+      {/* {errors.map(obj => {
+        <p>{obj}</p>
+      })} */}
+      
     </div>
   );
 }
---
+++ src/context/posterContext.js
@@ -0,0 +1,29 @@
+import React, {createContext, useReducer} from "react"
+
+export const INITIAL_STATE = {
+    newsType: "",
+    title: "",
+    authorName: "",
+    authorImage: "",
+    themeImage: "",
+}
+
+export const posterContext = createContext(INITIAL_STATE)
+
+const PosterReducer = (state,action) => {
+    switch (action.type){
+        case "set_poster_obj":
+            return action.payload
+        default:
+            return state
+    }
+}
+
+export const PosterContextProvider = ({children}) =>{
+    const [state,dispatch] = useReducer(PosterReducer,INITIAL_STATE)
+    return (
+        <posterContext.Provider value={{state,dispatch}}>
+            {children}
+        </posterContext.Provider> 
+    )
+}
